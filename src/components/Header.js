9a63a4eed2e3574d85f0ee2a8f33e490a2545ff0
sapla1125 <sapla1125@gmail.com>
2022-01-15 14:33:45 -0500

    Navbar Section

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import "./Header.css";
+import SearchIcon from '@mui/icons-material/Search';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+
+function Header() {
+    return (
+        <div className='header'>
+            
+            <img className='header__logo' 
+            src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' alt="Amazon" />
+            <LocationOnIcon className='header__locationLogo' />
+             <div className='header__location'>
+                    
+                    <span className='header__optionLineOne'>Hello</span>
+                    <span className='header__optionLineTwo'>Select Your Address</span>
+                </div>
+
+            <div className='header__search'>
+                <input className='header__searchInput' type="text" />
+                <SearchIcon className="header__searchIcon" />
+                
+            </div>
+            <div className='header__nav' >
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Hello Guest</span>
+                    <span className='header__optionLineTwo'>Sign In</span>
+                </div>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Returns</span>
+                    <span className='header__optionLineTwo'>& Orders</span>
+                </div>
+                <div className="header__optionBasket">
+                    <ShoppingCartIcon className="header__baseketLogo"/>
+                    <span className='header__optionLineTwo header__basketCount'>0</span>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Header;
