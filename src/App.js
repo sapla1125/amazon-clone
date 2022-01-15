5490c8d573147fcdcae3751e1bb1ba4a834295ed
sapla1125 <sapla1125@gmail.com>
2022-01-15 18:00:53 -0500

    Home Screen Hero Image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       {/* Header */}
       <Header />     
       {/* Home */}
+      <Home />
 
     </div>
   );
---
+++ src/components/home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className='home'>
+            <div className='home__container'>
+                <img className='home__image' src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020/TV/BRND/BRND_MTH20_00000_GWBleedingHero_1500x600_Final_en-US_PVD5676._CB404165680_.jpg" alt='Logo Banner' />
+            </div>
+            <div className='home__row'>
+                {/* Product */}
+                {/* Product */}
+            </div>
+            <div className='home__row'>
+                {/* Product */}
+                {/* Product */}
+                {/* Product */}
+            </div>
+            <div className='home__row'>
+                {/* Product */}
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home
