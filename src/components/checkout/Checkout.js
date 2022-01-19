a07cf30860e018e6ec0a916a2afabe762c7722ee
sapla1125 <sapla1125@gmail.com>
2022-01-19 17:50:10 -0500

    Checkout Page

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import "./Checkout.css";
 import Subtotal from '../subtotal/Subtotal';
+import CheckoutProduct from '../checkoutProducts/CheckoutProduct';
+import { useStateValue } from '../../StateProvider';
 
 function Checkout() {
+    const [{basket}, dispatch] = useStateValue();
     return (
         <div className='checkout'>
             <div className='checkout__left'>
@@ -14,6 +17,17 @@ function Checkout() {
                     <h2 className='checkout__title'>
                         Your Shopping Basket
                     </h2>
+                        {basket.map(item => (
+                            <CheckoutProduct 
+                                if={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        ))}
+                    
+
                 </div>
             </div>
             <div className='checkout__right'>
