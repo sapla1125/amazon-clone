2e3cbc6e3d7996692527a3e52a1bf02b6f51063d
sapla1125 <sapla1125@gmail.com>
2022-01-19 17:14:01 -0500

    Add to cart functionality

--- src/components/subtotal/Subtotal.js
+++ src/components/subtotal/Subtotal.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from '../../StateProvider';
+import { getBasketTotal } from '../../reducer';
 
 function Subtotal() {
+    const [{basket}, dispatch] = useStateValue();
+
+
     return (
         <div className='subtotal'>
             <CurrencyFormat
@@ -10,8 +15,8 @@ function Subtotal() {
                     <>
                         <p>
                                 {/* HW */}
-                            Subtotal (0 items):
-                            <strong>0</strong>
+                            Subtotal ({basket.length} items):
+                            <strong>{getBasketTotal(basket)}</strong>
                         </p>
                         <small className='subtotal__gift'>
                             <input type="checkbox" /> This order contains a gift
@@ -19,7 +24,7 @@ function Subtotal() {
                     </>
                 )}
                 decimalScale={2}
-                value={0} //HW
+                value={getBasketTotal(basket)} //HW
                 displayType={"text"}
                 thousandSeparator={true}
                 prefix={"$"}
